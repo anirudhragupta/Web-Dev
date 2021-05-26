a9982fd436fbe6cbe86b0524982a5faf22a9d116
Ani <anirudhragupta110@gmail.com>
2021-05-27 03:25:16 +0530

    did the dice challenge

---
+++ front-end/js/challenges/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.ceil(Math.random()*6);
+var randomNumber2 = Math.ceil(Math.random()*6);
+function dicePick(n){
+    if(n===1){
+        return "dice1.png";
+    }else if(n===2){
+        return "dice2.png";
+    }else if(n===3){
+        return "dice3.png";
+    }else if(n===4){
+        return "dice4.png";
+    }else if(n===5){
+        return "dice5.png";
+    }else{
+        return "dice6.png";
+    }
+}
+
+var pic1 = "images/" + dicePick(randomNumber1);
+var pic2 = "images/" + dicePick(randomNumber2);
+document.querySelector(".img1").setAttribute("src", pic1);
+document.querySelector(".img2").setAttribute("src", pic2);
