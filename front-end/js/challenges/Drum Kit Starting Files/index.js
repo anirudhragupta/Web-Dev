a7d03b36c15accdd0d2188171d5c79302182d1a7
Ani <anirudhragupta110@gmail.com>
2021-05-28 11:28:54 +0530

    got introduced to constructors

--- front-end/js/challenges/Drum Kit Starting Files/index.js
+++ front-end/js/challenges/Drum Kit Starting Files/index.js
@@ -1,11 +1,17 @@
 var numOfDrums = (document.querySelectorAll(".drum").length);
 
 for( var i=0;i<=numOfDrums;i++){
-    document.querySelectorAll("button")[i].addEventListener("click",clicked);
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+
+        this.style.color = "white";
+
+        // var drumAudio = new Audio("sounds/tom-1.mp3");
+        // drumAudio.play();
+    })
 }
 
-function clicked(){
-    alert("I got clicked");
-}
+// function clicked(){
+//     alert("I got clicked");
+// }
 
 
