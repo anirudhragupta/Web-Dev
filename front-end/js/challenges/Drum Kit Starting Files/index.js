3e7d279968b02c35c289b3acb72040520d3b2448
Ani <anirudhragupta110@gmail.com>
2021-05-29 06:16:18 +0530

    js debugging sucks

--- front-end/js/challenges/Drum Kit Starting Files/index.js
+++ front-end/js/challenges/Drum Kit Starting Files/index.js
@@ -1,17 +1,61 @@
 var numOfDrums = (document.querySelectorAll(".drum").length);
 
-for( var i=0;i<=numOfDrums;i++){
+//for button clicks
+for(var i=0;i<numOfDrums;i++){
     document.querySelectorAll("button")[i].addEventListener("click", function () {
-
-        this.style.color = "white";
-
-        // var drumAudio = new Audio("sounds/tom-1.mp3");
-        // drumAudio.play();
-    })
+        
+        makeSound(this.innerHTML);
+        //this.style.color = "white";
+        /* var drumAudio = new Audio("sounds/tom-1.mp3");
+        drumAudio.play();*/
+    });
 }
 
-// function clicked(){
-//     alert("I got clicked");
-// }
+//to capture key strokes
+document.addEventListener("keydown", function(eve){
+    console.log(eve.key);
+    makeSound(eve.key);
+});
 
+//making sound
+function makeSound(keys){
+    
+    switch(keys){
+        case "w":
+            var drumAudio = new Audio("sounds/crash.mp3");
+            drumAudio.play();
+        break;
+        case "a":
+            var drumAudio = new Audio("sounds/kick-bass.mp3");
+            drumAudio.play();
 
+        break;
+        case "s":
+            var drumAudio = new Audio("sounds/snare.mp3");
+            drumAudio.play();
+
+        break;
+        case "d":
+            var drumAudio = new Audio("sounds/tom-1.mp3");
+            drumAudio.play();
+
+        break;
+        case "j":
+            var drumAudio = new Audio("sounds/tom-2.mp3");
+            drumAudio.play();
+
+        break;
+        case "k":
+            var drumAudio = new Audio("sounds/tom-3.mp3");
+            drumAudio.play();
+
+        break;
+        case "l":
+            var drumAudio = new Audio("sounds/tom-4.mp3");
+            drumAudio.play();
+
+        break;
+        default:
+            console.log(this.innerHTML);
+    }
+}
